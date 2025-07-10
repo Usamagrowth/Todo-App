dbcf1e4332b447e75c0445559a78f9a4138eb34b
Usamagrowth <usamaomodara@gmail.com>
2025-07-10 15:26:15 +0100

    todo-app

---
+++ index.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const todoList = document.getElementById("todo-lists");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You most enter your task!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    todoList.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+    saveData();
+  }
+  inputBox.value = "";
+  saveData();
+}
+
+todoList.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", todoList.innerHTML);
+}
+function showTask() {
+  todoList.innerHTML = localStorage.getItem("data");
+}
+showTask();
