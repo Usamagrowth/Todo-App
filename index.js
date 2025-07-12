6773afd1f4e24e3c9b9812ed4a400bd99a1c9beb
Usamagrowth <usamaomodara@gmail.com>
2025-07-12 17:02:11 +0100

    todo-app

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const inputBox = document.getElementById("input-box");
 const todoList = document.getElementById("todo-lists");
+const form = document.getElementById("add-form")
 
-function addTask() {
+form.addEventListener("submit", addTask)
+function addTask(e) {
+  e.preventDefault()
   if (inputBox.value === "") {
     alert("You most enter your task!");
   } else {
@@ -30,6 +33,15 @@ todoList.addEventListener(
   },
   false
 );
+
+function listToEdit(editingLi){
+  const listToEdit = todoList.filter((todoList) => {
+
+  if (todoList.list === editingLi) {
+    return todoList()
+  }
+  }
+)}
 
 function saveData() {
   localStorage.setItem("data", todoList.innerHTML);
